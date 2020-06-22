35e00ef9c4685e97da198ecf18637e92c09f1aba
prashanthi paladugu <prashanthipaladugu@prashanthis-MacBook-Pro.local>
2020-06-22 15:15:06 +0530

    Initial Commit

---
+++ src/app/Employee.ts
@@ -0,0 +1,9 @@
+export class Employee{
+    id: number;
+    firstName: string;
+    lastName: string;
+    emailId: string;
+    age: number;
+    salary: number;
+    active: boolean;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
